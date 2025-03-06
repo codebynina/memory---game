124c6ab69f50b80a7aacddf50027d6c09e1ce9f8
niña <ninamarieondevilla@gmail.com>
2025-03-06 07:37:35 +0100

    Added images for the Memory Game

---
+++ memoryGame.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const card = document.getElementById("card");
+  
+    card.addEventListener("click", () => {
+      card.classList.toggle("flipped");
+    });
+  });
+  
